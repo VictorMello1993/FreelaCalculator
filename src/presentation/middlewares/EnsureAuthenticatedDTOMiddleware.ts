00b2cd4706009df6f2d8cb8eeb573a5a54eca11f
Victor Mello <victor.mello@al.infnet.edu.br>
2022-09-25 15:36:55 -0300

    Ultimo ajustes

--- src/presentation/middlewares/EnsureAuthenticatedDTOMiddleware.ts
+++ src/presentation/middlewares/EnsureAuthenticatedDTOMiddleware.ts
@@ -4,24 +4,28 @@ import { getSubjectByToken } from "../../utils/auth.helpers";
 
 export const EnsureAuthenticatedDTOMiddleware = (model: any, httpContext: string) => {
   return async (req: any, res: any, next: any) => {
-    const output: any = plainToInstance(model, req[httpContext]);
+    try {
+      const output: any = plainToInstance(model, req[httpContext]);
 
-    const { authorization } = output;
+      const { authorization } = output;
 
-    if (!authorization) {
-      return res.status(401).json({ messages: "Token missing" });
+      if (!authorization) {
+        return res.status(401).json({ messages: "Token missing" });
+      }
+
+      const [, token] = authorization.split(" ");
+
+      if (!verify(token, process.env.SECRET_KEY)) {
+        return res.status(401).json({ messages: "Invalid token" });
+      }
+
+      const subject = getSubjectByToken(token);
+
+      req.user = { id: subject };
+
+      return next();
+    } catch (error) {
+      return res.status(401).json({ messages: error.message });
     }
-
-    const [, token] = authorization.split(" ");
-
-    if (!verify(token, process.env.SECRET_KEY)) {
-      return res.status(401).json({ messages: "Invalid token" });
-    }
-
-    const subject = getSubjectByToken(token);
-
-    req.user = { id: subject };
-
-    return next();
   };
 };
